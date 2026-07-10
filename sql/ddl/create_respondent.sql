06117335a7c70585d3ff414dc95204d3b98b3f2d
peteperezlv <peteperez.lv@gmail.com>
2026-07-10 22:44:43 +0000

    added gender column

--- sql/ddl/create_respondent.sql
+++ sql/ddl/create_respondent.sql
@@ -1,7 +1,7 @@
 CREATE TABLE respondent (
     respondent_id INT PRIMARY KEY,
-    respondent_name STRING,
-    respondent_description STRING, 
+    respondent_description STRING,
+    gender STRING,
     start_date DATE,
     end_date DATE,
     active BOOLEAN
