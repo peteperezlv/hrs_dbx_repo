bd66667074a67ff966bb191f44c3d1bead64f1e9
peteperezlv <peteperez.lv@gmail.com>
2026-07-10 19:28:53 +0000

    initial script to create the wave reference table.

---
+++ sql/ddl/create_wave.sql
@@ -0,0 +1,10 @@
+CREATE TABLE wave (
+    wave_id INT PRIMARY KEY,
+    wave_name STRING,
+    wave_description STRING, 
+    start_date DATE,
+    end_date DATE,
+    active BOOLEAN
+)
+USING DELTA
+COMMENT 'Wave tracking table';
