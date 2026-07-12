cec2c2974b5774b5e887a43a80b7be54adb4084b
peteperezlv <peteperez.lv@gmail.com>
2026-07-12 15:27:59 +0000

    update cohort schema ddl

--- sql/ddl/create_cohort.sql
+++ sql/ddl/create_cohort.sql
@@ -2,11 +2,14 @@ DROP TABLE IF EXISTS dev_catalog.slv_cdm_hrs.cohort;
 
 CREATE TABLE dev_catalog.slv_cdm_hrs.cohort (
     cohort_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    cohort_name STRING,
+    hacohort DOUBLE,
+    cohort_label STRING,
     cohort_description STRING,
-    start_date DATE,
-    end_date DATE,
-    active BOOLEAN
+    create_date DATE,
+    update_date DATE,
+    active BOOLEAN,
+    wave_id BIGINT,
+     CONSTRAINT fk_wave_lookup FOREIGN KEY (wave_id) REFERENCES dev_catalog.slv_cdm_hrs.wave(wave_id)
 )
 USING DELTA
 COMMENT 'Cohort tracking table';
