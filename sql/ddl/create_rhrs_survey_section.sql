581bbd680ed783130ab865e774c43ef924666488
peteperezlv <peteperez.lv@gmail.com>
2026-07-13 20:52:18 +0000

    new ddl for the Rand HRS Longitudinal sections.

---
+++ sql/ddl/create_rhrs_survey_section.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS dev_catalog.slv_cdm_hrs.rhrs_survey_section;
+
+CREATE TABLE dev_catalog.slv_cdm_hrs.rhrs_survey_section (
+    rhrs_survey_section_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    rhrs_survey_section_name STRING,
+    rhrs_survey_section_description STRING, 
+    start_date DATE,
+    end_date DATE,
+    active BOOLEAN
+)
+USING DELTA
+COMMENT 'rhrs_survey_section tracking table';
