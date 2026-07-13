89966308b323bf64554290c6006d6fbc3bd5395d
peteperezlv <peteperez.lv@gmail.com>
2026-07-13 22:02:11 +0000

    change table name to sync with Rand codebook

--- sql/ddl/create_respondent.sql
+++ sql/ddl/create_respondent.sql
@@ -2,15 +2,18 @@ DROP TABLE IF EXISTS dev_catalog.slv_cdm_hrs.respondent;
 
 CREATE TABLE dev_catalog.slv_cdm_hrs.respondent (
     respondent_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cohort_id BIGINT,
+    respondent_type_id BIGINT,
+    wave_id BIGINT,
     respondent_description STRING,
     gender STRING,
-    respondent_type_id BIGINT,
-    cohort_id BIGINT,
+    respondent_type STRING,
     start_date DATE,
     end_date DATE,
     active BOOLEAN,
     CONSTRAINT fk_respondent_type FOREIGN KEY (respondent_type_id) REFERENCES dev_catalog.slv_cdm_hrs.respondent_type(respondent_type_id),
-    CONSTRAINT fk_cohort FOREIGN KEY (cohort_id) REFERENCES dev_catalog.slv_cdm_hrs.cohort(cohort_id)
+    CONSTRAINT fk_cohort FOREIGN KEY (cohort_id) REFERENCES dev_catalog.slv_cdm_hrs.cohort(cohort_id),
+    CONSTRAINT fk_wave_id FOREIGN KEY (wave_id) REFERENCES dev_catalog.slv_cdm_hrs.wave(wave_id)
 )
 USING DELTA
 COMMENT 'Respondent tracking table';
--- sql/ddl/create_survey_response.sql
+++ sql/ddl/create_survey_response.sql
@@ -4,13 +4,13 @@ CREATE TABLE dev_catalog.slv_cdm_hrs.question_response (
     question_response_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     question_response_description STRING,
     respondent_id BIGINT,
-    question_id BIGINT,
+    survey_variable_id BIGINT,
     wave_id BIGINT,
-    start_date DATE,
-    end_date DATE,
+    create_date DATE,
+    update_date DATE,
     active BOOLEAN,
     CONSTRAINT fk_respondent FOREIGN KEY (respondent_id) REFERENCES dev_catalog.slv_cdm_hrs.respondent(respondent_id),
-    CONSTRAINT fk_question FOREIGN KEY (question_id) REFERENCES dev_catalog.slv_cdm_hrs.question(question_id),
+    CONSTRAINT fk_question FOREIGN KEY (survey_variable_id) REFERENCES dev_catalog.slv_cdm_hrs.survey_variable(survey_variable_id),
     CONSTRAINT fk_wave FOREIGN KEY (wave_id) REFERENCES dev_catalog.slv_cdm_hrs.wave(wave_id)
 )
 USING DELTA
