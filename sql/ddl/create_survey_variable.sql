97507a9ead407cc725d68832d14dbee8efd15bae
peteperezlv <peteperez.lv@gmail.com>
2026-07-14 20:31:07 +0000

    updated the survey variable verification code.

--- sql/ddl/create_survey_variable.sql
+++ sql/ddl/create_survey_variable.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS dev_catalog.slv_cdm_hrs.survey_variable;
 CREATE TABLE dev_catalog.slv_cdm_hrs.survey_variable (
     survey_variable_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     rhrs_survey_section_id BIGINT,
-    wave_id BIGINT,
+    hrs_wave_id BIGINT,
     survey_variable_name STRING,
     survey_variable_description STRING,
     survey_variable_type STRING,
     create_date DATE,
     update_date DATE,
     active BOOLEAN,
-    CONSTRAINT survey_variable_wave_fk FOREIGN KEY (wave_id) REFERENCES dev_catalog.slv_cdm_hrs.wave(wave_id),
+    CONSTRAINT survey_variable_wave_fk FOREIGN KEY (hrs_wave_id) REFERENCES dev_catalog.slv_cdm_hrs.hrs_wave(hrs_wave_id),
     CONSTRAINT survey_variable_rhrs_survey_section_fk FOREIGN KEY (rhrs_survey_section_id) REFERENCES dev_catalog.slv_cdm_hrs.rhrs_survey_section(rhrs_survey_section_id)
 )
 USING DELTA
--- sql/validation/verify_survey_variable.sql
+++ sql/validation/verify_survey_variable.sql
@@ -1,3 +1,18 @@
-SELECT *
-FROM dev_catalog.slv_cdm_hrs.survey_variable
-ORDER BY survey_variable_id
+-- Verify Survey Variable Data
+-- Displays all survey variables with wave year information
+
+SELECT 
+    sv.survey_variable_id,
+    sv.rhrs_survey_section_id,
+    sv.hrs_wave_id,
+    w.wave_number,
+    w.wave_year,
+    sv.survey_variable_name,
+    sv.survey_variable_description,
+    sv.survey_variable_type,
+    sv.create_date,
+    sv.update_date,
+    sv.active
+FROM dev_catalog.slv_cdm_hrs.survey_variable sv
+INNER JOIN dev_catalog.slv_cdm_hrs.hrs_wave w ON sv.hrs_wave_id = w.hrs_wave_id
+ORDER BY sv.survey_variable_id;
