34c7c49de70fb73d116fa0ce91fe524d7166d81c
peteperezlv <ipeteperez@hotmail.com>
2026-07-26 12:10:30 -0700

    Updated 20_load_hrs_respondent_data notebook

--- sql/dml/load_hrs_respondent_data.sql
+++ sql/dml/load_hrs_respondent_data.sql
@@ -1,13 +1,13 @@
 -- Load Survey Respondent Reference Data 
 -- Target: dev_catalog.slv_cdm_hrs.hrs_survey_respondent
 -- hrs_survey_respondent_id is auto-generated (IDENTITY column)
-INSERT INTO dev_catalog.slv_cdm_hrs.hrs_survey_respondent (
+INSERT INTO dev_catalog.slv_cdm_hrs.hrs_respondent (
         cohort_id,
         hhid,
         pn,
         hhidpn,
         hacohort_number,
-        survey_respondent_description,
+        respondent_description,
         create_date,
         update_date,
         active
@@ -17,7 +17,7 @@ SELECT DISTINCT CAST(CAST(hhidpn AS BIGINT) AS STRING) as hhidpn,
     CAST(CAST(pn AS BIGINT) AS STRING) as pn,
     c.cohort_id,
     CAST(HACOHORT AS INT) as hacohort_number,
-    NULL as survey_respondent_description,
+    NULL as respondent_description,
     CURRENT_DATE() as create_date,
     CURRENT_DATE() as update_date,
     true as active
