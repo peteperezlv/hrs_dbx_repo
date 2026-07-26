dde0b5b6cec9ceb4fc137f7b0445d0c197d4a15a
peteperezlv <ipeteperez@hotmail.com>
2026-07-26 11:05:45 -0700

    refactored the sql_utils.py function

--- src/common/sql_utils.py
+++ src/common/sql_utils.py
@@ -1,3 +1,21 @@
+#This python app provides a common funtion that can be used to execute multi-SQL-statement files.
+
+#**Purpose: 
+    # Handle .sql files with multiple SQL statements.
+
+#**Function Name: 
+    # execute_sql_file
+
+#**Function Arguments**
+    #  spark - The active SparkSession.
+    #  sql_file - The name of the .sql file with one or more SQL statements.
+    #  display_results - True or False to direct the function how to handle the 'display' statement
+
+#**Process:**
+    # Step 1 - Set the sql path variable and check if it exists.
+    # Step 2 - Read the SQL file statements and create an array of SQL statements.
+    # Step 3 - Use the enumerate function to loop throgh each SQL statement and execute it.
+
 from pathlib import Path
 
 def execute_sql_file(spark, sql_file, display_results=False):
@@ -5,6 +23,7 @@ def execute_sql_file(spark, sql_file, display_results=False):
     Execute one or more SQL statements in a .sql file.
     """
 
+    # Step 1 - Set the sql path variable and check if it exists.
     sql_path = Path(sql_file)
 
     print("\n======================================")
@@ -16,6 +35,7 @@ def execute_sql_file(spark, sql_file, display_results=False):
             f"SQL file not found: {sql_path.resolve()}"
     )
 
+    # Step 2 - Read the SQL file statements and create an array of SQL statements.
     sql_text = sql_path.read_text()
 
     statements = []
@@ -28,9 +48,10 @@ def execute_sql_file(spark, sql_file, display_results=False):
 
     print(f"Found {len(statements)} SQL statement(s).\n")
 
+    # Step 3 - Use the enumerate function to loop throgh each SQL statement and execute it.
     for i, statement in enumerate(statements, start=1):
 
-        print(f"Executing statement {i}/{len(statements)}")
+        print(f"\nExecuting statement {i}/{len(statements)}")
 
         result = spark.sql(statement)
 
