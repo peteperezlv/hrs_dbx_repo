e863a04784ce5c633f35d93fc46c4c02f73f36bc
peteperezlv <ipeteperez@hotmail.com>
2026-07-26 01:05:36 -0700

    refactored the sql_utils.py function

--- src/common/sql_utils.py
+++ src/common/sql_utils.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 def execute_sql_file(spark, sql_file, display_results=False):
-    
-    # Execute one or more SQL statements in a .sql file.
+    """
+    Execute one or more SQL statements in a .sql file.
+    """
 
     sql_path = Path(sql_file)
 
@@ -10,7 +11,6 @@ def execute_sql_file(spark, sql_file, display_results=False):
 
     sql_text = sql_path.read_text()
 
-    # Create an array of SQL statements
     statements = []
 
     for stmt in sql_text.split(";"):
@@ -21,14 +21,12 @@ def execute_sql_file(spark, sql_file, display_results=False):
 
     print(f"Found {len(statements)} SQL statement(s).\n")
 
-    # Execute each SQL statement in the array.
     for i, statement in enumerate(statements, start=1):
 
         print(f"Executing statement {i}/{len(statements)}")
 
-        result = spark.sql(statement) # execute the statement
+        result = spark.sql(statement)
 
-        # Display the results.  display() is a databricks function
         if display_results:
             try:
                 display(result)
@@ -36,32 +34,3 @@ def execute_sql_file(spark, sql_file, display_results=False):
                 result.show(truncate=False)
 
     print(f"\n✓ Completed: {sql_path.name}")
-
-
-
-
-
-#from pathlib import Path
-
-#def execute_sql_file(spark, sql_file):
- #   """
- #   Execute one or more SQL statements from a .sql file.
-
-    #Parameters
-   # ----------
-    #spark : SparkSession
-    #    Active Spark session.
-
-    #sql_file : str
-    #    Path to the SQL file.
-    #"""
-
-    #sql_text = Path(sql_file).read_text()
-
-    #statements = sql_text.split(";")
-
-    #for statement in statements:
-    #    statement = statement.strip()
-#
- #       if statement:
-  #          spark.sql(statement)
