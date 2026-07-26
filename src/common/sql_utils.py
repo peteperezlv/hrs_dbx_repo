2056fa0f4c52fa37ff8f2d9e4456f087ec6f1564
peteperezlv <ipeteperez@hotmail.com>
2026-07-26 01:38:23 -0700

    refactored the sql_utils.py function

--- src/common/sql_utils.py
+++ src/common/sql_utils.py
@@ -8,6 +8,7 @@ def execute_sql_file(spark, sql_file, display_results=False):
     sql_path = Path(sql_file)
 
     #print(f"\nExecuting SQL file: {sql_path.name}")
+    print("hello")
     print("\n======================================")
     print(f"SQL File Provided : {sql_file}")
     print(f"SQL File Resolved : {sql_path.resolve()}")
