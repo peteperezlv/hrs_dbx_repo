1341672ea32c7a064d78de9c3015c7b15a636e25
peteperezlv <ipeteperez@hotmail.com>
2026-09-03 20:46:30 -0700

    updated load respondent sql to include respondent_descripition values

--- sql/dml/load_hrs_respondent_data.sql
+++ sql/dml/load_hrs_respondent_data.sql
@@ -19,7 +19,25 @@ SELECT DISTINCT c.cohort_id,
     CAST(CAST(pn AS BIGINT) AS STRING) as pn,
     CAST(hhidpn AS BIGINT) as hhidpn,
     CAST(HACOHORT AS INT) as hacohort,
-    NULL as respondent_description,
+    CONCAT(
+        CASE
+            WHEN ra.RARACEM = 1 THEN 'White'
+            WHEN ra.RARACEM = 2 THEN 'Black'
+            WHEN ra.RARACEM = 3 THEN 'Other'
+            ELSE ' - '
+        END,
+        ', ',
+        CASE
+            WHEN ra.RAGENDER = 1 THEN 'Male'
+            WHEN ra.RAGENDER = 2 THEN 'Female'
+            ELSE ' - '
+        END,
+        ', ',
+        CASE
+            WHEN ra.RAVETRN = 1 THEN 'Veteran'
+            ELSE 'Non-Veteran'
+        END
+    ) as respondent_description,
     CURRENT_DATE() as create_date,
     CURRENT_DATE() as update_date,
     true as active
