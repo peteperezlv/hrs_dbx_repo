190421ccb75134f7bf0f93963430f14aed0c8324
peteperezlv <ipeteperez@hotmail.com>
2026-09-04 11:12:53 -0700

    refactoring sql ddl to replace dynamic variables with simple drop and create sql"

--- sql/ddl/create_hrs_cohort.sql
+++ sql/ddl/create_hrs_cohort.sql
@@ -1,20 +1,7 @@
 -- comments
-DROP TABLE IF EXISTS IDENTIFIER(
-    CONCAT(
-        :catalog_name,
-        '.',
-        :schema_prefix,
-        '.dim_cohort'
-    )
-);
-CREATE TABLE IDENTIFIER(
-    CONCAT(
-        :catalog_name,
-        '.',
-        :schema_prefix,
-        '.dim_cohort'
-    )
-) (
+DROP TABLE IF EXISTS dev_catalog.slv_cdm_hrs.dim_cohort;
+--
+CREATE TABLE dev_catalog.slv_cdm_hrs.dim_cohort (
     cohort_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     hacohort_number INT,
     hacohort_label STRING,
